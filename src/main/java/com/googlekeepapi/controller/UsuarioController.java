0d5bec78466ce61d4e81387364625c6bebc00823
victorloboc <engvictorlobo@hotmail.com>
2021-03-20 11:53:41 -0300

    método para excluir usuário: ok

--- src/main/java/com/googlekeepapi/controller/UsuarioController.java
+++ src/main/java/com/googlekeepapi/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -59,6 +60,20 @@ public class UsuarioController {
 			return ResponseEntity.ok(new UsuarioDto(usuario));	
 		}
 		
+		return ResponseEntity.notFound().build();
+	}
+	
+	//EXCLUIR USUARIO
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> excluirUsuario(@PathVariable Long id) {
+		
+		Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+		if(usuarioOptional.isPresent()) {
+			usuarioRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
 		return ResponseEntity.notFound().build();
 	}
 	
